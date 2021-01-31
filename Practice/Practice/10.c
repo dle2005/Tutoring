c5246424974173a6a2df7e9424a007845980983a
DongKon Lee <dle0129@gmail.com>
2021-01-31 14:02:02 +0900

    week_2 answer created

---
+++ Practice/Practice/10.c
@@ -0,0 +1,11 @@
+/*
+#include <stdio.h>
+int main() {
+	for(int i=32; i<126; i+=5) {
+		for(int j=0; j<5; j++) {
+			printf("%3d-%3c   ",i+j,i+j);
+		}
+		printf("\n");
+	}
+}
+*/
---
+++ Practice/Practice/12.c
@@ -0,0 +1,26 @@
+/*
+#include <stdio.h>
+
+void main() {
+	int studentScore[10] = { 0 };
+	int studentRank[10] = { 1,1,1,1,1,1,1,1,1,1 };
+
+	for (int i = 0; i < 10; i++) {
+		printf("%d번 학생: ", i + 1);
+		scanf("%d", &studentScore[i]);
+	}
+
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			if (i == j) continue;
+
+			if (studentScore[i] < studentScore[j])
+				studentRank[i]++;
+		}
+	}
+
+	printf("--결과--\n");
+	for (int i = 0; i < 10; i++)
+		printf("%d번 학생: %d점 - %d등\n", i + 1, studentScore[i], studentRank[i]);
+}
+*/
